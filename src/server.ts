d26215726932d8021cc5ecf1866da5eecc6ac39a
lekan.dev <realjoy9@gmail.com>
2023-10-17 17:29:04 +0100

    draft: launch yjs websocket with express

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,33 @@
-import app from './app.js';
 import 'dotenv/config';
 import http from 'http';
-// import setUpWebSocket from './yServer.cjs';
-import { setupWSConnection } from 'y-websocket/bin/utils';
-// import connectDB from './connectDB.js';
-// import scheduleMatch from './scheduleMatch.js';
+import expressWs from 'express-ws';
+
+import app from './app.js';
+import { type WebSocket } from 'ws';
+import { type Request } from 'express';
+// @ts-expect-error
+import * as yWebsocket from '../node_modules/y-websocket/bin/utils.js';
 
 const PORT = process.env.PORT ?? 8000;
+// connect y-websocekt server to express app
 
 const server = http.createServer(app);
+const wsInstance = expressWs(app, server);
+
+wsInstance.app.ws('/yjs', (ws: WebSocket, req: Request) => {
+  yWebsocket.setupWSConnection(ws, req);
+  console.log('y-websocket connected', ws.url);
+});
+
+// console.log(wsInstance, 'wsInstance');
 
 async function startServer (): Promise<void> {
-  // await setUpWebSocket(server);
-
   server.listen(PORT, () => { console.log(`running on port : ${PORT}`); });
 }
 
 await startServer();
-// Initialize y-websocket server
-setupWSConnection(server, {
-  path: '/yjs' // specify a custom path for Yjs WebSocket connections
-});
 
-// Your existing error handling and server listening logic
-// server.listen(PORT, () => {
-//   console.log(`Server running on port: ${PORT}`);
-// });
+// handle unhandled rejections
 
 process.on('bad auth', (err: unknown) => {
   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
